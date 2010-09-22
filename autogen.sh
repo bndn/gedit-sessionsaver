49e32668df2eaea589b650ca7ee5aef9c4c306dc
Javier Jardón <jjardon@gnome.org>
2010-09-22 19:19:19 +0200

    build: Use upstream gettext instead the Glib one

--- autogen.sh
+++ autogen.sh
@@ -17,4 +17,6 @@ which gnome-autogen.sh || {
     exit 1
 }
 
+autopoint --force || exit $?
+
 ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4" REQUIRED_AUTOMAKE_VERSION=1.9 REQUIRED_MACROS=python.m4 USE_GNOME2_MACROS=1 . gnome-autogen.sh
