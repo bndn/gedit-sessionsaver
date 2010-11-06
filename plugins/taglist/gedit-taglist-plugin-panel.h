afa6a4dcbe2f72df50cf48c6eebed1d08bda4843
Paolo Borelli <pborelli@gnome.org>
2010-11-06 21:32:20 +0100

    Import the taglist plugin from gedit
    Move the taglist plugins here for those who like it. These days it
    should be way less popular than it used to, so we decided to remove it
    from the main gedit tarball in 3.0.

---
+++ plugins/taglist/gedit-taglist-plugin-panel.h
@@ -0,0 +1,73 @@
+/*
+ * gedit-taglist-plugin-panel.h
+ * This file is part of gedit
+ *
+ * Copyright (C) 2005 - Paolo Maggi 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA 02111-1307, USA.
+ */
+ 
+/*
+ * Modified by the gedit Team, 2005. See the AUTHORS file for a 
+ * list of people on the gedit Team.  
+ * See the ChangeLog files for a list of changes.
+ */
+
+#ifndef __GEDIT_TAGLIST_PLUGIN_PANEL_H__
+#define __GEDIT_TAGLIST_PLUGIN_PANEL_H__
+
+#include <gtk/gtk.h>
+
+#include <gedit/gedit-window.h>
+
+G_BEGIN_DECLS
+
+#define GEDIT_TYPE_TAGLIST_PLUGIN_PANEL              (gedit_taglist_plugin_panel_get_type())
+#define GEDIT_TAGLIST_PLUGIN_PANEL(obj)              (G_TYPE_CHECK_INSTANCE_CAST((obj), GEDIT_TYPE_TAGLIST_PLUGIN_PANEL, GeditTaglistPluginPanel))
+#define GEDIT_TAGLIST_PLUGIN_PANEL_CONST(obj)        (G_TYPE_CHECK_INSTANCE_CAST((obj), GEDIT_TYPE_TAGLIST_PLUGIN_PANEL, GeditTaglistPluginPanel const))
+#define GEDIT_TAGLIST_PLUGIN_PANEL_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST((klass), GEDIT_TYPE_TAGLIST_PLUGIN_PANEL, GeditTaglistPluginPanelClass))
+#define GEDIT_IS_TAGLIST_PLUGIN_PANEL(obj)           (G_TYPE_CHECK_INSTANCE_TYPE((obj), GEDIT_TYPE_TAGLIST_PLUGIN_PANEL))
+#define GEDIT_IS_TAGLIST_PLUGIN_PANEL_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), GEDIT_TYPE_TAGLIST_PLUGIN_PANEL))
+#define GEDIT_TAGLIST_PLUGIN_PANEL_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS((obj), GEDIT_TYPE_TAGLIST_PLUGIN_PANEL, GeditTaglistPluginPanelClass))
+
+typedef struct _GeditTaglistPluginPanel		GeditTaglistPluginPanel;
+typedef struct _GeditTaglistPluginPanelPrivate	GeditTaglistPluginPanelPrivate;
+typedef struct _GeditTaglistPluginPanelClass	GeditTaglistPluginPanelClass;
+
+struct _GeditTaglistPluginPanel 
+{
+	GtkVBox vbox;
+
+	/*< private > */
+	GeditTaglistPluginPanelPrivate *priv;
+};
+
+struct _GeditTaglistPluginPanelClass 
+{
+	GtkVBoxClass parent_class;
+};
+
+GType 		 gedit_taglist_plugin_panel_get_type		(void) G_GNUC_CONST;
+
+GtkWidget	*gedit_taglist_plugin_panel_new 		(GeditWindow *window,
+								 const gchar *data_dir);
+
+void		_gedit_taglist_plugin_panel_register_type	(GTypeModule *type_module);
+
+G_END_DECLS
+
+#endif  /* __GEDIT_TAGLIST_PLUGIN_PANEL_H__  */
+/* ex:ts=8:noet: */
---
+++ plugins/taglist/gedit-taglist-plugin.h
@@ -0,0 +1,69 @@
+/*
+ * gedit-taglist-plugin.h
+ * 
+ * Copyright (C) 2002-2005 - Paolo Maggi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * Modified by the gedit Team, 2002-2005. See the AUTHORS file for a 
+ * list of people on the gedit Team.  
+ * See the ChangeLog files for a list of changes.
+ */
+ 
+#ifndef __GEDIT_TAGLIST_PLUGIN_H__
+#define __GEDIT_TAGLIST_PLUGIN_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <libpeas/peas-extension-base.h>
+#include <libpeas/peas-object-module.h>
+
+G_BEGIN_DECLS
+
+#define GEDIT_TYPE_TAGLIST_PLUGIN		(gedit_taglist_plugin_get_type ())
+#define GEDIT_TAGLIST_PLUGIN(o)			(G_TYPE_CHECK_INSTANCE_CAST ((o), GEDIT_TYPE_TAGLIST_PLUGIN, GeditTaglistPlugin))
+#define GEDIT_TAGLIST_PLUGIN_CLASS(k)		(G_TYPE_CHECK_CLASS_CAST((k), GEDIT_TYPE_TAGLIST_PLUGIN, GeditTaglistPluginClass))
+#define GEDIT_IS_TAGLIST_PLUGIN(o)		(G_TYPE_CHECK_INSTANCE_TYPE ((o), GEDIT_TYPE_TAGLIST_PLUGIN))
+#define GEDIT_IS_TAGLIST_PLUGIN_CLASS(k)	(G_TYPE_CHECK_CLASS_TYPE ((k), GEDIT_TYPE_TAGLIST_PLUGIN))
+#define GEDIT_TAGLIST_PLUGIN_GET_CLASS(o)	(G_TYPE_INSTANCE_GET_CLASS ((o), GEDIT_TYPE_TAGLIST_PLUGIN, GeditTaglistPluginClass))
+
+typedef struct _GeditTaglistPlugin		GeditTaglistPlugin;
+typedef struct _GeditTaglistPluginPrivate	GeditTaglistPluginPrivate;
+typedef struct _GeditTaglistPluginClass		GeditTaglistPluginClass;
+
+struct _GeditTaglistPlugin
+{
+	PeasExtensionBase parent_instance;
+
+	/*< private >*/
+	GeditTaglistPluginPrivate *priv;
+};
+
+struct _GeditTaglistPluginClass
+{
+	PeasExtensionBaseClass parent_class;
+};
+
+GType			gedit_taglist_plugin_get_type	(void) G_GNUC_CONST;
+
+G_MODULE_EXPORT void	peas_register_types		(PeasObjectModule *module);
+
+G_END_DECLS
+
+#endif /* __GEDIT_TAGLIST_PLUGIN_H__ */
+
+/* ex:ts=8:noet: */
