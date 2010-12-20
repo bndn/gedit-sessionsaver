2788cbc65f182a03f79cdf155d86bed5f990f42b
Ignacio Casal Quinteiro <icq@gnome.org>
2010-12-20 22:50:03 +0100

    Add missing copyright.

--- plugins/charmap/charmap/panel.py
+++ plugins/charmap/charmap/panel.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010 Ignacio Casal Quinteiro <icq@gnome.org>
+# Copyright (C) 2006 Steve Frécinaux <steve@istique.net>
+#               2010 Ignacio Casal Quinteiro <icq@gnome.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
