fcd3c40ff6cc897519cd12ae852f7c2af8f2e264
Ignacio Casal Quinteiro <icq@gnome.org>
2010-12-20 21:56:15 +0100

    Port charmap plugin to python.

---
+++ plugins/charmap/charmap/__init__.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Ignacio Casal Quinteiro <icq@gnome.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+from gi.repository import GObject, Gio, Pango, Gtk, Gedit, Gucharmap
+from panel import CharmapPanel
+import sys
+
+class CharmapPlugin(GObject.Object, Gedit.WindowActivatable):
+    __gtype_name__ = "CharmapPlugin"
+
+    window = GObject.property(type=GObject.Object)
+
+    def __init__(self):
+        GObject.Object.__init__(self)
+
+    def do_activate(self):
+        self.editor_settings = Gio.Settings.new("org.gnome.gedit.preferences.editor")
+        self.editor_settings.connect("changed::use-default-font", self.font_changed)
+        self.editor_settings.connect("changed::editor-font", self.font_changed)
+        self.system_settings = Gio.Settings.new("org.gnome.desktop.interface")
+        self.system_settings.connect("changed::monospace-font-name", self.font_changed)
+
+        panel = self.window.get_side_panel()
+        theme = Gtk.IconTheme.get_default()
+
+        if theme.has_icon("accessories-character-map"):
+            image = Gtk.Image.new_from_icon_name("accessories-character-map", Gtk.IconSize.MENU)
+        else:
+            image = Gtk.Image.new_from_icon_name("gucharmap", Gtk.IconSize.MENU)
+
+        self.create_charmap_panel()
+        panel.add_item(self.panel, "GeditCharmapPanel", _("Character Map"), image)
+
+        statusbar = self.window.get_statusbar()
+        self.context_id = statusbar.get_context_id("Character Description")
+
+    def do_deactivate(self):
+        panel = self.window.get_side_panel()
+        panel.remove_item(self.panel)
+
+    def do_update_state(self):
+        pass
+
+    def get_document_font(self):
+        if self.editor_settings.get_boolean("use-default-font"):
+            font = self.system_settings.get_string("monospace-font-name")
+        else:
+            font = self.editor_settings.get_string("editor-font")
+
+        return font
+
+    def font_changed(self, settings=None, key=None):
+        font = self.get_document_font()
+        font_desc = Pango.font_description_from_string(font)
+
+        chartable = self.panel.get_chartable()
+        chartable.set_font_desc(font_desc)
+
+    def create_charmap_panel(self):
+        self.panel = CharmapPanel()
+        chartable = self.panel.get_chartable()
+
+        # Use the same font as the document
+        self.font_changed()
+
+        chartable.connect("notify::active-character", self.on_table_sync_active_char)
+        chartable.connect("focus-out-event", self.on_table_focus_out_event)
+        chartable.connect("status-message", self.on_table_status_message)
+        chartable.connect("activate", self.on_table_activate)
+
+        self.panel.show()
+
+    def on_table_sync_active_char(self, chartable, pspec):
+        uc = chartable.get_active_character()
+        text = "%s %s" % (uc, Gucharmap.get_unicode_name(uc))
+
+        a = Gucharmap.get_nameslist_equals(uc)
+        if a:
+            text += "   = %s" % a[0]
+            for i in range(len (a) - 1):
+                text += "; %s" % a[i + 1]
+
+        a = Gucharmap.get_nameslist_stars(uc)
+        if a:
+            text += "   \342\200\242 %s" % a[0]
+            for i in range(len (a) - 1):
+                text += "; %s" % a[i + 1]
+
+        self.on_table_status_message(chartable, text)
+
+    def on_table_focus_out_event(self, chartable, event):
+        self.on_table_status_message (chartable, None)
+
+        return False
+
+    def on_table_status_message(self, chartable, message):
+        statusbar = self.window.get_statusbar()
+
+        statusbar.pop(self.context_id)
+
+        if message:
+            statusbar.push(self.context_id, message)
+
+    def on_table_activate(self, chartable):
+        uc = chartable.get_active_character()
+        if not Gucharmap.unichar_validate(uc):
+            raise ValueError
+
+        view = self.window.get_active_view()
+        if not view or not view.get_editable():
+            return
+
+        document = view.get_buffer()
+
+        document.begin_user_action()
+        iters = document.get_selection_bounds()
+        if iters:
+            document.delete_interactive(iters[0], iters[1], True)
+            start = iters[0]
+        else:
+            insert = document.get_insert()
+            start = document.get_iter_at_mark(insert)
+
+        if start.editable(True):
+            document.insert(start, uc)
+
+        document.end_user_action()
+
+# ex:et:ts=4:
---
+++ plugins/charmap/charmap/panel.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Ignacio Casal Quinteiro <icq@gnome.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+from gi.repository import Gtk, Gucharmap
+
+class CharmapPanel(Gtk.Box):
+    __gtype_name__ = "CharmapPanel"
+
+    def __init__(self):
+        Gtk.Box.__init__(self)
+
+        paned = Gtk.Paned.new(Gtk.Orientation.VERTICAL)
+
+        scrolled_window = Gtk.ScrolledWindow(None, None)
+        scrolled_window.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scrolled_window.set_shadow_type(Gtk.ShadowType.ETCHED_IN)
+
+        self.view = Gucharmap.ChaptersView()
+        self.view.set_headers_visible (False)
+
+        model = Gucharmap.ScriptChaptersModel()
+        self.view.set_model(model)
+
+        selection = self.view.get_selection()
+        selection.connect("changed", self.on_chapter_view_selection_changed)
+
+        scrolled_window.add(self.view)
+        paned.pack1(scrolled_window, False, True)
+
+        scrolled_window = Gtk.ScrolledWindow(None, None)
+        scrolled_window.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scrolled_window.set_shadow_type(Gtk.ShadowType.ETCHED_IN)
+
+        self.chartable = Gucharmap.Chartable()
+
+        scrolled_window.add(self.chartable)
+        paned.pack2(scrolled_window, True, True)
+
+        self.view.select_locale()
+
+        paned.set_position(150)
+        paned.show_all()
+
+        self.pack_start(paned, True, True, 0)
+
+    def on_chapter_view_selection_changed(self, selection):
+        model, it = selection.get_selected()
+        if it:
+            codepoint_list = self.view.get_codepoint_list()
+            self.chartable.set_codepoint_list(codepoint_list)
+
+    def get_chartable(self):
+        return self.chartable
+
+# ex:et:ts=4:
