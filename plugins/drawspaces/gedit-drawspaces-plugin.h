fa8af54d3319b8a64b30d1ddc0010d681c9e3226
Dominique Leuenberger <dimstar@opensuse.org>
2012-08-19 13:19:13 +0200

    trivial: update FSF Address in source files\nFixes bug 682187

--- plugins/drawspaces/gedit-drawspaces-plugin.h
+++ plugins/drawspaces/gedit-drawspaces-plugin.h
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * $Id: gedit-drawspaces-plugin.h 137 2006-04-23 15:13:27Z sfre $
  */
--- plugins/taglist/gedit-taglist-plugin.c
+++ plugins/taglist/gedit-taglist-plugin.c
@@ -15,16 +15,14 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 /*
  * Modified by the gedit Team, 2002-2005. See the AUTHORS file for a 
  * list of people on the gedit Team.  
  * See the ChangeLog files for a list of changes. 
- *
- * $Id$
  */
 
 #ifdef HAVE_CONFIG_H
